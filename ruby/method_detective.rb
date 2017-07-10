9a83ecb7a8a2dcba6463efb790fd1268ee18d4e0
Simon Brady <simonbrady@mac.com>
2017-07-10 13:26:24 -0400

    properly upload, failed to do so earlier, made corrections

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,38 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+# "iNvEsTiGaTiOn".<swapcase>
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+# "zom".<insert(1,"o">
 # => “zoom”
 
-# "enhance".<???>
+# "enhance".center("enhance".length + 8, " ")
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+# "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+# "the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+# " suspects".insert("the usual')
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+# "The case of the disappearing last letter".pop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+# "The mystery of the missing first letter".<???> (This one i need help with, is there a singular method? could we .reverse, then delete the last character, then reverse it back?)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+# "Elementary,    my   dear        Watson!".split.join("  ")
 # => "Elementary, my dear Watson!"
+puts "Elementary,    my   dear        Watson!".split.join("  ")
 
-# "z".<???>
+# "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
-
-# "How many times does the letter 'a' appear in this string?".<???>
+# 122 is the ASCII code for the letter 'z'
+puts "z".ord
+# "How many times does the letter 'a' appear in this string?".count('a')
 # => 4
