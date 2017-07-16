bca1c27d46e10eb57d78a318dc18055957d4ec7d
Simon Brady <simonbrady@mac.com>
2017-07-16 11:01:14 -0400

    Add basic Santa attributes & test driver code

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,9 @@
 class Santa
-	def initialize
-		puts "initializing Santa instance"
+	def initialize(name, ethnicity)
+		@name = name
+		@ethnicity = ethnicity
+		@age = 0
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 	end
 
 	def speak
@@ -11,3 +14,12 @@ class Santa
 		puts "That was a good #{cookie_type}"
 	end
 end
+
+santas = []
+santas << Santa.new("agender", "black")
+santas << Santa.new("female", "Latino")
+santas << Santa.new("bigender", "white")
+santas << Santa.new("male", "Japanese")
+santas << Santa.new("female", "prefer not to say")
+santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+santas << Santa.new("N/A", "N/A")
