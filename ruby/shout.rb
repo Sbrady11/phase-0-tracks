588c52beb75a7dfe47c04a07c85776a4474619d1
Simon Brady <simonbrady@mac.com>
2017-07-17 08:25:28 -0400

    Create shout Mixin, add driver code to test for police & coach

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,33 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yelling_happily(words)
-  	words + "!!!" + " :D"
-  end
+#   def self.yelling_happily(words)
+#   	words + "!!!" + " :D"
+#   end
+# end
+
+# puts Shout.yell_angrily("I hate mondays")
+
+# puts Shout.yelling_happily("I love coding")
+
+module Shout
+	def shout(words)
+		puts words.upcase
+	end
 end
 
-puts Shout.yell_angrily("I hate mondays")
+class Police
+	include Shout
+end
 
-puts Shout.yelling_happily("I love coding")
+class Coach
+	include Shout
+end
+
+gary = Police.new
+puts gary.shout("You're under arrest")
+
+larry = Coach.new
+puts larry.shout("Touchdown")
