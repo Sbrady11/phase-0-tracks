91551f77a231c7a404800e88ee434d8bd05b5049
Simon Brady <simonbrady@mac.com>
2017-07-17 11:49:39 -0400

    Added reflection question answers, whoops

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -109,3 +109,27 @@ update_quantity(test_food_list, 'ice cream', 1)
 print_pretty(test_food_list)
 
 puts "********************************************************************"
+
+# What did you learn about pseudocode from working on this challenge?
+
+	# That there are quite a lot of different styles that you can use, but go for readability, ALWAYS!
+
+# What are the tradeoffs of using arrays and hashes for this challenge?
+	
+	# Strange question as we used what was necessary to complete the assignment, in that the array was used to take in a string, then, as more information was needed, we applied said string to a hash format.
+
+# What does a method return?
+
+	# The last line of code that you pass in, or your return statement.
+
+# What kind of things can you pass into methods as arguments?
+
+	# Lots of things! Any type of value, or even other methods and objects!
+
+# How can you pass information between methods?
+	
+	# Through the creation of a variable the holds onto the output from different methods.
+
+# What concepts were solidified in this challenge, and what concepts are still confusing?
+
+	# Honestly the way that we passed info from method to method was solidified, as for confusion, none so far. Yet I am going to try and refractor this a wee bit, especially with my newfound knowledge of instance variables and scope.
