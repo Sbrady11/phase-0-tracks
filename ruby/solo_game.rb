84fe3ae7dcca5969351b35bef75abf36b6d9e89a
Simon Brady <simonbrady@mac.com>
2017-07-17 12:54:29 -0400

    Update cases with winning and losing conditions, build loop until win or lose, hide computer input

--- ruby/solo_game.rb
+++ ruby/solo_game.rb
@@ -12,21 +12,33 @@ class Game
 		computer_hand = @roshambo_options.sample
 	end
 
-	def check_hands(computer_hand)
+	def check_hands(player_hand, computer_hand)
 		case [player_hand, computer_hand]
-		when ['rock', 'scissors'], ['scissors', 'paper']
-			puts "you win!"
+		when ['rock', 'scissors'], ['scissors', 'paper'], ['paper', 'rock']
+			puts "A winrar is you, GAME OGRE"
 			@is_over = true
-		when ['scissors', 'rock'], ['paper', 'scissors']
-			puts "Whoops, you lose, again?"
+		when ['scissors', 'rock'], ['paper', 'scissors'], ['rock', 'paper']
+			puts "You Lose! GAME OGRE"
 			@is_over = true
 		when player_hand == computer_hand
-			puts "Tie!"
 		end
+	end
 end
-
-
 
 game = Game.new
 
-game.generate_computer_hand
+while !game.is_over
+	computer_hand = game.generate_computer_hand
+  
+  puts "enter your hand! ('rock', 'paper', or 'scissors')"
+  player_hand = gets.chomp
+	
+  game.check_hands(player_hand, computer_hand)
+end
+
+puts "Computer threw a #{computer_hand}"
+
+
+
+
+
