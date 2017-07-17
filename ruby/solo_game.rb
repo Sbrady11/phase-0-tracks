543a68f78b69e38c9c3ca97e3f64fe391f248873
Simon Brady <simonbrady@mac.com>
2017-07-17 12:38:42 -0400

    Build rock paper scissor cases

--- ruby/solo_game.rb
+++ ruby/solo_game.rb
@@ -1,5 +1,5 @@
 class Game
-	attr_accessor :player_hand, :computer_hand
+	attr_accessor :player_hand, :computer_hand, :is_over
 
 	def initialize
 		@player_hand = player_hand
@@ -11,6 +11,18 @@ class Game
 	def generate_computer_hand
 		computer_hand = @roshambo_options.sample
 	end
+
+	def check_hands(computer_hand)
+		case [player_hand, computer_hand]
+		when ['rock', 'scissors'], ['scissors', 'paper']
+			puts "you win!"
+			@is_over = true
+		when ['scissors', 'rock'], ['paper', 'scissors']
+			puts "Whoops, you lose, again?"
+			@is_over = true
+		when player_hand == computer_hand
+			puts "Tie!"
+		end
 end
 
 
