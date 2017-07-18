fd6642058915c52c54eb2a2cc3ff193de669df9f
Simon Brady <simonbrady@mac.com>
2017-07-18 13:38:04 -0400

    Create game class, initialize the end condition, guess counter, secret word, character checker & build hide method for the word

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -14,9 +14,55 @@
 # Repeat until the word is chosen or the player runs out of guesses
 # Word will be revealed regardless
 # Much fun will be had
+class WordGame
+	attr_reader :guess_count, :is_over
+	attr_accessor :hidden_word, :character_to_check, :secret_word
 
+	def initialize
+		@is_over = false
+		@guess_count = 0
+		@secret_word = secret_word
+		@character_to_check = character_to_check
+		puts "Starting game..."
+	end
+
+	def hide(word_to_hide)
+		@hidden_word = p ' _ ' * word_to_hide.length	
+		@hidden_word
+	end
+
+	def checker(character)
+	end
+
+end
+
+game = WordGame.new
+
+puts "Enter word to hide!"
 secret_word = gets.chomp
 
-hidden_word = p ' _ ' * secret_word.length	
+game.hide(secret_word)
+puts "You have #{secret_word.length} guesses remaining! Enter a character to check, or enter 'guess' to guess the secret word!"
+character_to_check = gets.chomp.downcase
+	# if character_to_check == 'guess'
 
-puts hidden_word
+	# elsif character_to_check.length > 1
+	# 	puts "Please just a letter to check at first!"
+	# 	character_to_check = gets.chomp
+
+game.checker(character_to_check)
+
+
+
+# hidden_word = p ' _ ' * secret_word.length	
+
+# puts hidden_word
+
+# puts hidden_word[1]
+
+
+# puts secret_word[1]
+
+# hidden_word[1] = 's'
+
+# puts hidden_word
