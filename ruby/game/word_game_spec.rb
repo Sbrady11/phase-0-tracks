50945577401cf710874062cd0bae80057d27fa55
Simon Brady <simonbrady@mac.com>
2017-07-20 16:25:40 -0400

    Add tests, confusing...

--- ruby/game/word_game_spec.rb
+++ ruby/game/word_game_spec.rb
@@ -3,7 +3,17 @@ require_relative 'word_game'
 describe WordGame do 
 	let (:game) {WordGame.new}
 
-	expect(game.start).to eq true 
+	it "takes in a word to check" do
+		expect(game.initialize("word"))
+	end
+
+	it "checks characters" do
+		expect(checker('w'))
+	end
+
+	it "checks if a character is valid" do
+		expect(game.character_is_valid?('i')).to eq true
+	end
 
 
 end
