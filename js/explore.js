356158e1899602a32e5c0f820ef87ed8830fda98
Simon Brady <simonbrady@mac.com>
2017-07-22 15:53:07 -0400

    Create reverse function for javascript

---
+++ js/explore.js
@@ -0,0 +1,12 @@
+// Reverse Function PseudoCode!
+// Take in a string
+// Take the length of the string, iterate over the string starting from the end
+// Add the letter to a new string
+// Print the string
+
+function reverse(word) {
+	var new_str = ""
+	for (var i = word.length - 1; i >= 0; i--){
+		new_str += word[i]
+	}
+}
