1ce6bafee253b541cb48a155ff9159da9ddc71fc
Simon Brady <simonbrady@mac.com>
2017-07-24 12:21:45 -0400

    Write pseudocode for second function

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,6 @@
-// Longest Array function
+// Longest Array function------------------------------
+ 
+// Psuedocode
 // Take in the array of phrases
 // Initialize the search variable (the number comparing the strings I.e, start it at 0, pass the first array length, compare)
 	// If the new phrase is longer, store that phrase as the current longest, compare with the next, and repeat
@@ -22,4 +24,16 @@ var testArray1 = ["duck", "cow"]
 console.log(longestPhrase(testArray))
 console.log(longestPhrase(testArray1))
 
+//Find a key-value match function----------------------
+//Takes in two objects, checks to see if the objects share at least one key value pair, returns true or false
+
+//Psuedocode
+// Take in two objects 
+// Determine if key 1 and key 2 in object one and two are equivalent
+// If yes, return true
+// Else, return false
+
+function objectMatcher(obj1, obj2) {
+	
+}
 
