3d0b201544c455d29f1d0b18a4def7cb7934dbb0
Simon Brady <simonbrady@mac.com>
2017-07-24 12:13:54 -0400

    Correct with camel casing, used to ruby..

--- js/algos.js
+++ js/algos.js
@@ -3,8 +3,17 @@
 // Initialize the search variable (the number comparing the strings I.e, start it at 0, pass the first array length, compare)
 	// If the new phrase is longer, store that phrase as the current longest, compare with the next, and repeat
 // Once looped through, return the longest array
-function longest_phrase(array) {
-	var longest_string = ""
+function longestPhrase(array) {
+	var longestString = ""
 	var searchLength = 0
 
+	for (var i = 0; i < array.length; i++){
+		if (array[i].length > searchLength){
+			searchLength = array[i].length;
+			longestString = array[i];
+		}
+	}
+	return longestString;
 }
+
+var testArray = ["long phrase","longest phrase","longer phrase"]
