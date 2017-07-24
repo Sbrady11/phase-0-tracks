72c5ed722579850be9245cdc3cd99cb9cf2467ff
Simon Brady <simonbrady@mac.com>
2017-07-24 11:25:59 -0400

    Add reflection section, push to github

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -1,10 +1,11 @@
 # Virus Predictor
 
-# I worked on this challenge [by myself, with: ].
-# We spent [#] hours on this challenge.
+# I worked on this challenge [by myself, with: Lorraine].
+# We spent [1.5] hours on this challenge.
 
 # EXPLANATION OF require_relative
-#
+# links to file needed
+# require relative is subset of require - allows loading of files
 #
 require_relative 'state_data'
 
@@ -17,45 +18,48 @@ class VirusPredictor
   end
 # returns combined virus output (predicted_deaths and speed_of_spread) from two methods
   def virus_effects
-    predicted_deaths(@population_density, @population, @state)
-    speed_of_spread(@population_density, @state)
+    predicted_deaths
+    speed_of_spread
   end
-# makes this next block of code inaccessible for outside objects 
+
+# makes this next block of code inaccessible for outside objects
   private
 # takes in population_density, population, & state, determines appropriate calculation, outputs state death totals
-  def predicted_deaths(population_density, population, state)
+  def predicted_deaths
     # predicted deaths is solely based on population density
+
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      multiplier = 0.4
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      multiplier = 0.3
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      multiplier = 0.2
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      multiplier = 0.1
     else
-      number_of_deaths = (@population * 0.05).floor
+      multiplier = 0.05
     end
+
+    number_of_deaths = (@population * multiplier).floor
 
     print "#{@state} will lose #{number_of_deaths} people in this outbreak"
 
   end
 # takes in population_density, & state, determines appropriate calculation, outputs speed of spread
-  def speed_of_spread(population_density, state) #in months
+  def speed_of_spread #in months
     # We are still perfecting our formula here. The speed is also affected
     # by additional factors we haven't added into this functionality.
-    speed = 0.0
 
     if @population_density >= 200
-      speed += 0.5
+      speed = 0.5
     elsif @population_density >= 150
-      speed += 1
+      speed = 1
     elsif @population_density >= 100
-      speed += 1.5
+      speed = 1.5
     elsif @population_density >= 50
-      speed += 2
+      speed = 2
     else
-      speed += 2.5
+      speed = 2.5
     end
 
     puts " and will spread across the state in #{speed} months.\n\n"
@@ -89,3 +93,13 @@ end
 
 #=======================================================================
 # Reflection Section
+
+# The hash for the state_data file uses both the hash rocket and the symbol method. The hash rocket is for readability, while the symbol is for simplicity.
+
+# Require relative finds the file within the same folder, while require can take a longer address.
+
+# You can use the .each, or the .map methods, both using a block code to iterate through each key and value.
+
+# That they had already been defined as instance variables in the initialization of the new instance, so the further definition was redundant.
+
+# I think I really got a better handle on instance variables.
