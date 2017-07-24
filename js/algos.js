ec12710baa3c32662bd8875957d8930211efa200
Simon Brady <simonbrady@mac.com>
2017-07-24 12:09:31 -0400

    Create longest phrase function pseudocode, begin actual code

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+// Longest Array function
+// Take in the array of phrases
+// Initialize the search variable (the number comparing the strings I.e, start it at 0, pass the first array length, compare)
+	// If the new phrase is longer, store that phrase as the current longest, compare with the next, and repeat
+// Once looped through, return the longest array
+function longest_phrase(array) {
+	var longest_string = ""
+	var searchLength = 0
+
+}
