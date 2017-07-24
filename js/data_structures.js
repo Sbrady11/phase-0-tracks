93de0d634d3e32e0445e82cca54d06ca21084931
Simon Brady <simonbrady@mac.com>
2017-07-24 11:41:45 -0400

    Build Car object, add honk function, print to console to test

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,7 +1,33 @@
+//Horse colors and names
 var colors = ["red", "blue", "green", "yellow"];
 
 var horse_names = ["Dexter", "Wilhelm", "Duncan", "Floyd"];
 
-colors.push("orange")
+colors.push("orange");
 
-horse_names.push("Sam")
+horse_names.push("Sam");
+
+//Horse maker
+var colored_horses = {};
+
+for (var i = 0; i <= horse_names.length; i++) {
+	colored_horses[horse_names[i]] = colors[i];
+}
+
+//Cars
+
+function Car(make, model, year){
+	this.make = make;
+	this.model = model;
+	this.year = year;
+
+	this.honk = function() {
+		console.log(model + ": aroogah")
+	}
+}
+
+var ml320 = new Car("Mercedes", "ML320", 2009)
+var defender = new Car("Land Rover", "Defender", 1967)
+
+ml320.honk();
+defender.honk();
