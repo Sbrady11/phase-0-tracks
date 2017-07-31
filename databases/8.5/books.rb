224ace08d09008c7a496ab37f31db1990f88d1f0
Simon Brady <simonbrady@mac.com>
2017-07-31 12:30:22 -0400

    Add string delimeter to build a book table

--- databases/8.5/books.rb
+++ databases/8.5/books.rb
@@ -3,3 +3,12 @@ require 'faker'
 
 db = SQLite3::Database.new("books.db")
 
+create_table_cmd = <<-SQL
+	CREATE TABLE books(
+		id INTEGER PRIMARY KEY,
+		title VARCHAR(255),
+		pages INT
+	);
+SQL
+
+db.execute(create_table_cmd)
