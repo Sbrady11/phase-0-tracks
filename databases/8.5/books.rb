2dccc37a47935c54b2a4290d6fbe947d6c7fbf7f
Simon Brady <simonbrady@mac.com>
2017-07-31 12:32:58 -0400

    Add conditional to not make extra tables or fail. Build test book

--- databases/8.5/books.rb
+++ databases/8.5/books.rb
@@ -4,7 +4,7 @@ require 'faker'
 db = SQLite3::Database.new("books.db")
 
 create_table_cmd = <<-SQL
-	CREATE TABLE books(
+	CREATE TABLE IF NOT EXISTS books(
 		id INTEGER PRIMARY KEY,
 		title VARCHAR(255),
 		pages INT
@@ -12,3 +12,5 @@ create_table_cmd = <<-SQL
 SQL
 
 db.execute(create_table_cmd)
+
+db.execute("INSERT INTO books (title, pages) VALUES ('Lord of the Flies', 200)")
