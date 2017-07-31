03ce63ff7de9e28bd42775da3fc51087538769a1
Simon Brady <simonbrady@mac.com>
2017-07-31 12:52:18 -0400

    Finish program to generate 100 random books a time

--- databases/8.5/books.rb
+++ databases/8.5/books.rb
@@ -2,6 +2,7 @@ require 'sqlite3'
 require 'faker'
 
 db = SQLite3::Database.new("books.db")
+db.results_as_hash = true
 
 create_table_cmd = <<-SQL
 	CREATE TABLE IF NOT EXISTS books(
@@ -14,3 +15,14 @@ SQL
 db.execute(create_table_cmd)
 
 db.execute("INSERT INTO books (title, pages) VALUES ('Lord of the Flies', 200)")
+
+def create_book(db, title, pages)
+	db.execute("INSERT INTO books (title, pages) VALUES (?,?)", [title, pages])
+
+end
+
+100.times do
+	create_book(db, Faker::Name.title, Faker::Number.between(100,3000))
+end
+
+
